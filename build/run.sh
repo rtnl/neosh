c02dc76a6135454a830d667375aa6f464742c611
snavy <1337snavy@gmail.com>
2025-03-08 18:31:52 +0100

    update run script

--- build/run.sh
+++ build/run.sh
@@ -4,5 +4,5 @@ set -e
 export WORKDIR=$(mktemp -d)
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib"
 
-cc -Wall -Wextra -fsanitize=address -g -o "${WORKDIR}/neosh" -lion ./src/*.c ./src/*.h
+cc -Wall -Wextra -fsanitize=address -lion -lssl -lcrypto -g -o "${WORKDIR}/neosh" ./src/*.c ./src/*.h
 "${WORKDIR}/neosh"
