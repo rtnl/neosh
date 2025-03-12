cdd60eb63650521106f8f1fa1803fa409d892b63
snavy <1337snavy@gmail.com>
2025-03-12 22:33:21 +0100

    update install script

--- build/install.sh
+++ build/install.sh
@@ -2,8 +2,7 @@
 set -e
 
 export WORKDIR=$(mktemp -d)
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib"
 
-cc -Wall -Wextra -lion -lssl -lcrypto -o "${WORKDIR}/neosh" ./src/*.c
+cargo build --release --target-dir "${WORKDIR}"
 
-sudo install "${WORKDIR}/neosh" /usr/local/bin
+sudo install "${WORKDIR}/release/neosh" /usr/local/bin
